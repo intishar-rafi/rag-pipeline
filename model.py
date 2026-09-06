f3381c9084e442ec263a4849d04af83ab5ec5ec3
Mohammed Intishar Rahman <152949789+intishar-rafi@users.noreply.github.com>
2026-09-06 08:39:55 +0200

    Step 31: track_source_chunk_ids

--- model.py
+++ model.py
@@ -369,8 +369,10 @@ def rag_answer(query, chunks, embeddings, embed_model, generator, tokenizer, k=3
 
     return {'answer': answer, 'sources': sources, 'query': query}
 
-# Step 31 - track_source_chunk_ids (not yet solved)
-# TODO: implement
+# Step 31 - track_source_chunk_ids
+def track_source_chunk_ids(source_chunks):
+    # TODO: return the list of chunk ids from the retrieved source chunks, preserving order
+    return [chunk['id'] for chunk in source_chunks if 'id' in chunk]
 
 # Step 32 - append_source_references (not yet solved)
 # TODO: implement
